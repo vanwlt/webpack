185851f74bfa0addf82235ef0e379e6755a1cc91
wlt <734490921@qq.com>
2022-06-27 22:57:48 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './banner.js'
 import './tabs.js'
 
-// import './styles/index.css'
+import './styles/index.css'
 
 import $ from 'jquery'
 
-$('#swiper').css('background-color', 'yellow')
+// $('#swiper').css('background-color', 'yellow')
